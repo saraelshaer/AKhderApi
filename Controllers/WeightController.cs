f69554ba4dc18c5a7fc9605d5e587c6ff1bfa486
saraelshaer <saraelshaer923@gmail.com>
2025-06-23 10:15:03 +0300

    update files

--- Controllers/WeightController.cs
+++ Controllers/WeightController.cs
@@ -1,6 +1,5 @@
 ﻿using AKhderApi.DTOs;
 using AKhderApi.Services;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AKhderApi.Controllers
@@ -9,29 +8,12 @@ namespace AKhderApi.Controllers
     [ApiController]
     public class WeightController : ControllerBase
     {
-        private readonly ICartService _cartService;
-        public WeightController(ICartService cartService)
-        {
-            _cartService = cartService;
-        }
+
 
         [HttpPost]
-        public async Task<IActionResult> CheckWeight([FromBody] WeightDto model)
+        public async Task<IActionResult> SendWeight([FromBody] WeightDto model)
         {
-            var expectedWeight = await _cartService.GetTotalWeight();
-            if (expectedWeight == 0)
-            {
-                return NotFound(new { message = "No products found in the cart." });
-            }
-
-            if (Math.Abs(model.ActualWeight - expectedWeight) < 0.5m) 
-            {
-                return Ok(new { message = "Weight matches the expected total weight." });
-            }
-            else
-            {
-                return BadRequest(new { message = $"Weight mismatch: Expected {expectedWeight} kg, but got {model.ActualWeight} kg." });
-            }
+            return Ok(new{ ActualWeight = model.ActualWeight});
         }
     }
 }
